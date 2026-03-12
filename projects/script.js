75ddc48ad1090ffc59ec492e62936a2eae761adc
Prakash Alagundagi <143861759+prakashalagundagi@users.noreply.github.com>
2026-03-12 23:27:22 +0530

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
+            document.title = "Projects | Portfolio Prakash A";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
@@ -135,3 +135,4 @@ document.onkeydown = function (e) {
         return false;
     }
 }
+
