3b5856a5c9d0b2e8b6fb197dceb58ef6f013251a
Prakash Alagundagi <143861759+prakashalagundagi@users.noreply.github.com>
2026-09-24 23:59:22 +0530

    bug fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
     $("#contact-form").submit(function (event) {
         emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
 
-        emailjs.sendForm('service_kwmvj2e', 'template_contact', '#contact-form')
+        emailjs.sendForm('service_kwmvj2e', 'template_swvux8s', '#contact-form')
             .then(function (response) {
                 console.log('SUCCESS!', response.status, response.text);
                 document.getElementById("contact-form").reset();
